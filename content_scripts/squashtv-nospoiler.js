b689695e752c49639d745e22e2147c26ac5f9a4d
James Judge <jamesjudge@warducks.com>
2022-04-13 10:06:10 +0100

    hiding video image in related videos making it possible to get the original video name on hover

--- content_scripts/squashtv-nospoiler.js
+++ content_scripts/squashtv-nospoiler.js
@@ -1,8 +1,15 @@
 for(var vid of document.querySelectorAll(".videoplayer__title, .item__title")){
-  vid.textContent = removePlayerNames(vid.textContent);
-  if(!!vid.parentElement.href){
-    var urlParts = vid.parentElement.href.split("/");
-    vid.textContent += "[" + urlParts[urlParts.length - 1] + "]";
+  // Skip for the video title of the one we're watching.
+  if(vid.parentElement.className.indexOf("page") == -1){ 
+    // Set actual title as tooltip
+    var originalTitle = vid.textContent;
+    vid.textContent = removePlayerNames(vid.textContent);
+    vid.title = originalTitle;
+
+    if(!!vid.parentElement.href){
+      var urlParts = vid.parentElement.href.split("/");
+      vid.textContent += "[" + urlParts[urlParts.length - 1] + "] -hover here for info-";
+    }
   }
 
   vid.style.display = "inline-block";
