488d8c56eef51a28bb51eb58167e93d6af57df1d
James Judge <jamesjudge@warducks.com>
2022-05-06 11:53:10 +0100

    removing unused file fixing contorl adding

--- js/squashtv-nospoiler-video.js
+++ js/squashtv-nospoiler-video.js
@@ -1,32 +1,40 @@
 var dailymotionPlayerRef;
 var currentTime;
 
-if(typeof variable !== 'undefined' && !!player) { 
-  player.then(p => {
-    dailymotionPlayerRef = p;
-  });
-
-  var vidControls = document.querySelectorAll(".videoplayer .container")[0];
-
-  if(!!vidControls){
-    var controls = document.createElement("div");
-    controls.className="squashtv-nospoiler-controls";
-
-    let timeInput = document.createElement('input');
-    timeInput.textContent = "0";
-
-    let btn = document.createElement('button');
-    btn.onclick = () => {
-      if(isNaN(timeInput.value)){
-        timeInput.value = "0";
+function addPlayerControls(){
+  if(typeof player !== 'undefined' && !!player) { 
+    player.then(p => {
+      dailymotionPlayerRef = p;
+    });
+  
+    var vidControls = document.querySelectorAll(".videoplayer .container")[0];
+  
+    if(!!vidControls){
+      var controls = document.createElement("div");
+      controls.className="squashtv-nospoiler-controls";
+  
+      let timeInput = document.createElement('input');
+      timeInput.textContent = "0";
+  
+      let btn = document.createElement('button');
+      btn.onclick = () => {
+        if(isNaN(timeInput.value)){
+          timeInput.value = "0";
+        }
+        else {
+          dailymotionPlayerRef.seek(timeInput.value);
+        }
       }
-      else {
-        dailymotionPlayerRef.seek(timeInput.value);
-      }
+      btn.textContent = "Set time in seconds";
+      controls.append(btn);
+      controls.append(timeInput);
+      vidControls.prepend(controls)
     }
-    btn.textContent = "Set time in seconds";
-    controls.append(btn);
-    controls.append(timeInput);
-    vidControls.prepend(controls)
+  }
+  else {
+    // retry
+    setTimeout(() => addPlayerControls(), 5000);
   }
 }
+
+addPlayerControls();
